fe129c03500d38727e883d1967f52e31bc44b686
Sébastien Gruhier <sebastien.gruhier@xilinus.com>
2018-02-25 19:03:53 +0100

    version 1.1
    Signed-off-by: Sébastien Gruhier <sebastien.gruhier@xilinus.com>

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,3 @@
-console.log("background script loaded.");
-
 // Listen for received message from content script and set to background scope globally
 chrome.runtime.onMessage.addListener(receiver);
 
